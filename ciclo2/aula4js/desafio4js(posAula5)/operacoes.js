ada3604fdeb700b4593f7585380f527e0c381995
Anderson sone <88639252+AndersonSone@users.noreply.github.com>
2021-08-25 21:32:24 -0300

    Add files via upload

---
+++ ciclo2/aula4js/desafio4js(posAula5)/operacoes.js
@@ -0,0 +1,52 @@
+window.onload=function(){
+
+
+   (()=>{
+    let mostrarProdutosCliente = document.querySelector("#content-produtos > ul#produtos");
+
+    for(let idx in produtos){
+        mostrarProdutosCliente.innerHTML += `<li class="itemProduto" data-preco=${produtos[idx].prodPreco}>${produtos[idx].prodDesc}`        
+    }
+   })(produtos) 
+
+
+///compra
+const itemProduto= document.querySelectorAll("#produtos > li.itemProduto");  
+const cestaDocliente =document.querySelector("ul#cestaDoCliente");
+const mostraTotalCompra = document.querySelector("#mostraTotalCompra");
+const armazenaItem=[];
+var totalPedido =0;
+itemProduto.forEach((item)=>{
+    item.addEventListener("click",()=>{
+
+        li = document.createElement("li");//  createElement = criou a lista!
+        li.setAttribute("data-preco", item.dataset.preco);//ele puxa o valor para nos consegui tira dps
+        //referencia para saber se tem um produto ja lançado = método indexOf()
+        if(armazenaItem.indexOf(item.textContent) == -1){
+            armazenaItem.push(item.textContent);
+            cestaDoCliente.appendChild(li).textContent = item.textContent = item.textContent;
+            totalPedido += Number(item.dataset.preco);
+            mostraTotalCompra.value = totalPedido.toLocaleString("pt-BR",
+            {style:"currency", currency: "BRL"})
+        }else{
+            alert(`Este item ${item.textContent} já está na sua cesta!`);
+        }
+    });
+});
+//aqui ele remove as coisas com 2 click
+ const cesta = document.querySelectorAll("#cestaDoCliente");//aqui que eu estava errando feio pq da para puxar usando classe do ul
+    const list = document.querySelector("ul#cestaDoCliente");
+    cesta.forEach((item) => {
+        item.addEventListener('dblclick', (itemCesta) => {
+            var idx = armazenaItem.indexOf(itemCesta.target.textContent);
+            if (idx > -1) {
+                totalPedido -= Number(itemCesta.target.dataset.preco);
+                cestaDocliente.removeChild(list.childNodes[idx]);
+                armazenaItem.splice(idx, 1);
+                mostraTotalCompra.value = totalPedido.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });//aqui para mostra valores em R$
+            }
+        });
+    });
+
+
+}
---
+++ ciclo2/aula4js/desafio4js(posAula5)/produtos.js
@@ -0,0 +1,11 @@
+var produtos=[
+	{prodDesc:"mamao papaia",prodPreco : 4.70 },
+	{prodDesc:"laranja",prodPreco : 3.70 },
+	{prodDesc: "Manga", prodPreco : 2.80 },
+    {prodDesc: "Melão", prodPreco : 6.90 },
+    {prodDesc: "banana", prodPreco : 5.00 },
+    {prodDesc: "limao", prodPreco : 7.00 },
+    {prodDesc: "Mexirica", prodPreco : 5.50 },
+    {prodDesc: "maca", prodPreco : 6.70 },
+
+];
